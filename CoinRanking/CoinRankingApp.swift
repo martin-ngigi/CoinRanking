525b2b3d570ceab09132298a114d9b04414243c2
wainaina-cb <mwainaina@co-opbank.co.ke>
2025-07-08 19:53:45 +0300

    Initial commit

---
+++ CoinRanking/CoinRankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRankingApp.swift
+//  CoinRanking
+//
+//  Created by Hummingbird on 08/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinRankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
